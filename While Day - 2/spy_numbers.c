2e339247ba5194d08035b90bfff2aea1b9fb4f72
Ankush Verma <ankushpandya488@gmail.com>
2024-10-09 19:24:50 +0530

    Spy numbers

---
+++ While Day - 2/spy_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0, product = 1, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    int temp = num; 
+
+    while (temp != 0) {
+        digit = temp % 10;
+        sum += digit;
+        product *= digit;
+        temp /= 10;
+    }
+
+    if (sum == product) {
+        printf("%d is a spy number\n", num);
+    } else {
+        printf("%d is not a spy number\n", num);
+    }
+
+    return 0;
+}
