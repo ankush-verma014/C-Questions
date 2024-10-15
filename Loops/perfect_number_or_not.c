d6872293879560a546bfccb8accdb1fd8864bac5
Ankush Verma <ankushpandya488@gmail.com>
2024-10-15 21:44:34 +0530

    loops

---
+++ Loops/perfect_number_or_not.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int number, sum = 0;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    for (int i = 1; i <= number / 2; i++) 
+    {
+        if (number % i == 0) 
+        {
+            sum += i;
+        }
+    }
+
+    if (sum == number) 
+    {
+        printf("%d is a perfect number.\n", number);
+    } else 
+    {
+        printf("%d is not a perfect number.\n", number);
+    }
+
+    return 0;
+}
