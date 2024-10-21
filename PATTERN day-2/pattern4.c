f1497d57d6a83298218a128e6a5dade67a1cb469
Ankush Verma <ankushpandya488@gmail.com>
2024-10-21 19:42:43 +0530

    pattern

--- PATTERN day-2/pattern4.c
+++ PATTERN day-2/pattern4.c
@@ -5,3 +5,19 @@
 // C  D  E  F
 // D  E  F  G
 
+#include <stdio.h>
+
+int main() {
+    int nr, nc, r, c;
+    char a;
+    scanf("%d %d", &nr, &nc);
+
+    for (r = 0; r <= nr; r++) {
+        a= 'A' +r;
+        for (c = 0; c <= nc; c++) {
+            printf("%c ", a++);
+        }
+        printf("\n");
+    }
+    return 0;
+}
