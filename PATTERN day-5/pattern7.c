ce77427ce77ec2ac8a4be7526598b94ace7a9c5a
Ankush Verma <ankushpandya488@gmail.com>
2024-10-25 22:15:12 +0530

    pattern

--- PATTERN day-5/pattern7.c
+++ PATTERN day-5/pattern7.c
@@ -16,34 +16,44 @@ int main() {
     
     scanf("%d",&nr);
     
-       for(int r=1; r<=nr; r++){
-            for(int c=1; c<=r; c++){
+       for(int r=1; r<=nr; r++)
+       {
+            for(int c=1; c<=r; c++)
+            {
                 printf("* ");
             }
-              for(int s=r; s<=nr; s++){
+              for(int s=r; s<=nr; s++)
+              {
                 printf("  ");
-            }
-              for(int s=r; s<nr; s++){
+              }
+              for(int s=r; s<nr; s++)
+              {
                 printf("  ");
-            }
-            for(int c=1; c<=r; c++){
+              }
+            for(int c=1; c<=r; c++)
+            {
                 printf("* ");
             }
             printf("\n");
         }
-         for(int r=1; r<nr; r++){
+         for(int r=1; r<nr; r++)
+        {
           
-            for(int c=r; c<nr; c++){
+            for(int c=r; c<nr; c++)
+            {
                 printf("* ");
             }
-               for(int s=1; s<=r; s++){
+               for(int s=1; s<=r; s++)
+              {
                 printf("  ");
-            }
+              }
               
-               for(int s=0; s<=r; s++){
-                printf("  ");
-            }
-           for(int c=r; c<nr; c++){
+               for(int s=0; s<=r; s++)
+               {
+                 printf("  ");
+               }
+           for(int c=r; c<nr; c++)
+            {
                 printf("* ");
             }
             printf("\n");
