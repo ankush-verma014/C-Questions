62f98a10f33a9e438c4d625c0c70b643362282be
Ankush Verma <ankushpandya488@gmail.com>
2024-10-25 10:10:15 +0530

    pattern

---
+++ PATTERN day-5/pattern6.c
@@ -0,0 +1,36 @@
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * * * 
+// * * * * 
+// * * * 
+// * * 
+// * 
+
+#include <stdio.h>
+
+int main() {
+   int nr,r,c;
+   
+   scanf("%d",&nr);
+   
+   for(r=1;r<=nr;r++)
+   {
+       for(c=1;c<=r;c++)
+       {
+           printf("* ");
+       }
+         printf("\n");
+   }
+      for(r=nr;r>=1;r--)
+   {
+       for(c=1;c<r;c++)
+       {
+           printf("* ");
+       }
+       printf("\n");
+   }
+
+    return 0;
+}
