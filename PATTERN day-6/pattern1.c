411d399f018e48f8a1d989fb4758d6b29d83510d
Ankush Verma <ankushpandya488@gmail.com>
2024-10-28 09:24:50 +0530

    pattern

--- PATTERN day-6/pattern1.c
+++ PATTERN day-6/pattern1.c
@@ -6,28 +6,51 @@
 //    *  *  *  *  *  *  *
 // *  *  *  *  *  *  *  *  *
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main() {
-    int nr, r, c;
+// int main() {
+//     int nr, r, c;
 
-    scanf("%d", &nr);
+//     scanf("%d", &nr);
 
-    for (r = 1; r <= nr; r++) 
-    {
+//     for (r = 1; r <= nr; r++) 
+//     {
       
-        for (c = 1; c <= nr - r; c++) 
-        {
-            printf("   ");
-        }
+//         for (c = 1; c <= nr - r; c++) 
+//         {
+//             printf("   ");
+//         }
         
-        for (c = 1; c <= (2 * r - 1); c++) 
-        {
-            printf("*  "); 
-        }
+//         for (c = 1; c <= (2 * r - 1); c++) 
+//         {
+//             printf("*  "); 
+//         }
         
-        printf("\n");
-    }
+//         printf("\n");
+//     }
   
+//     return 0;
+// }
+
+
+#include<stdio.h>
+int main() {
+    for(int i=1;i<=5;i++)
+    {
+          for(int j=5-1; j>=i; j--)
+          {
+               printf("  ");
+          }
+           for(int k=1;k<=i;k++)
+           {
+                printf("* ");
+           }
+            for(int l=1;l<=i-1;l++)
+            {
+               printf("* ");
+            }
+          printf("\n");
+     }
+
     return 0;
 }
