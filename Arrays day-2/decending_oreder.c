805c256a30c9951b67bd924fd1170870edaaeaf4
Ankush Verma <ankushpandya488@gmail.com>
2024-10-30 10:08:17 +0530

    arrays

---
+++ Arrays day-2/decending_oreder.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, temp;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int a[n];
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+      printf("Array elements are:\n");
+    for (i = 0; i < n; i++) {
+        printf("Index %d: %d\n", i, a[i]);
+    }
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (a[j] < a[j + 1]) {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Ascending order: ");
+        for (i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
