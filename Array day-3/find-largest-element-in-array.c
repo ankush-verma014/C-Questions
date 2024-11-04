6e8333ffdc368705e291242c43ad9b720fa60dff
Ankush Verma <ankushpandya488@gmail.com>
2024-11-04 16:31:48 +0530

    array

--- Array day-3/find-largest-element-in-array.c
+++ Array day-3/find-largest-element-in-array.c
@@ -1,16 +1,60 @@
-#include <stdio.h>
+// #include <stdio.h>
  
-int main() {
-    int arr[] = {30, 15, 25, 40, 20};
-    int max = arr[0];
+// int main() {
+//     int arr[] = {3,4,1,9,8,88,9,88,88,1};
+//     int max = arr[0];
      
-    for (int i = 1; i < 5; i++) {
+//     for (int i = 1; i < 5; i++) {
+//         if (arr[i] > max) {
+//             max = arr[i];
+//         }
+//     }
+     
+//     printf(" second Largest element in array: %d\n", max);
+     
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main() {
+    int n;
+
+   
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+   
+    printf("Enter the elements of the array: \n");
+    for (int i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = arr[0];
+    int second_max = -1;
+
+    
+    for (int i = 1; i < n; i++) 
+    {
         if (arr[i] > max) {
-            max = arr[i];
+            second_max = max;
+            max = arr[i]; 
+        } else if (arr[i] > second_max && arr[i] < max) 
+        {
+            second_max = arr[i]; 
         }
     }
-     
-    printf("Largest element in array: %d\n", max);
-     
+
+    if (second_max != -1) 
+    {
+        printf("Second largest element in array: %d\n", second_max);
+    } else 
+    {
+        printf("No second largest element found.\n");
+    }
+
     return 0;
 }
