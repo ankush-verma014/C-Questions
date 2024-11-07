4446380ca943f7eccfc8dc17ef2165b7d924991c
Ankush Verma <ankushpandya488@gmail.com>
2024-11-07 22:25:29 +0530

    string

--- string day 1/vowel-digit-constant-special.c
+++ string day 1/vowel-digit-constant-special.c
@@ -1,84 +1,54 @@
-// #include <stdio.h>
-// #include<ctype.h>
-// int main() {
-//     char s[100];
-//     int i, vowels = 0, consonants = 0, digits = 0, special = 0;
-   
-//     printf("Enter a string: ");
-//     scanf(" %[^\n]", s); 
+#include<stdio.h>
+int main (){
+    char s[100];
+    int i,v=0,c=0,spe=0,d=0,spa=0;
     
-
-//     for(i = 0; s[i] != '\0'; i++) 
-//     {
-//         if(isalpha(s[i])) 
-//         {
-        
-//             char ch = tolower(s[i]);
-//             if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') 
-//             {
-//                 vowels++;
-//             } else 
-//             {
-//                 consonants++;
-//             }
-//         } else if(isdigit(s[i])) 
-//         {
-//             digits++;
-//         } else 
-//         {
-//             special++;
-//         }
-//     }
+    printf("Enter a string:");
+    scanf("%[^\n]",s);
     
-//     printf("Vowel count is : %d\n", vowels);
-//     printf("Consonant count is : %d\n", consonants);
-//     printf("Digit count is : %d\n", digits);
-//     printf("Special Character count is : %d\n", special);
-    
-//     return 0;
-// }
-
-#include <stdio.h>
-
-int main()
-{
-    int vol=0,col=0,sp=0,spec=0,digit=0;
-    char str[100];
-    
-    printf("Enter a string: ");
-    scanf("%[^\n]s",str);
-    
-    
-    // int length = 0;
-
-    // for (int i = 0; str[length] != '\0'; i++)
-    // {
-    //     length++;
-    // };
-    
-    for(int i=0; str[i]!='\0'; i++){
-        if(str[i]>='A'&&str[i]<='Z'){
-          if(str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'){
-              vol++;
-          }else{
-              col++;
-          }
-        }else if(str[i]>='a'&& str[i]<='z'){
-           if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'){
-              vol++;
-          }else{
-              col++;
-          }
-        }else if(str[i]==' '){
-            sp++;
-        }else if(str[i]>='0'&&str[i]<='9'){
-            digit++;
-            }else{
-            spec++;
+    for(i=0;s[i]!='\0';i++)
+    {
+        if(s[i]>='a' && s[i]<='z')
+        {
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+            {
+                v++;
+            }
+            else
+            {
+                c++;
+            }
+        }
+        else if(s[i]>='A'&& s[i]<='Z')
+        {
+            if(s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
+            {
+                v++;
+            }
+            else
+            {
+                c++;
+            }
+        }
+        else if(s[i]>='0'&&s[i]<='9')
+        {
+            d++;
+        }
+        else if(s[i]==' ')
+        {
+            spa++;
+        }
+        else
+        {
+            spe++;
         }
     }
     
-    printf("vowel= %d\nconsonant= %d\nspace= %d\nspecial character= %d\nDigit= %d",vol, col, sp, spec,digit);
+    printf("vowel=%d\n",v);
+    printf("Consonant=%d\n",c);
+    printf("digit=%d\n",d);
+    printf("space=%d\n",spa);
+    printf("special=%d\n",spe);
     
     return 0;
 }
