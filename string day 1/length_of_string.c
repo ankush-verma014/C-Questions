8b77c1222af5612a0b7ab0890a8378cceb99a5c2
Ankush Verma <ankushpandya488@gmail.com>
2024-11-07 18:49:24 +0530

    string

--- string day 1/length_of_string.c
+++ string day 1/length_of_string.c
@@ -1,10 +1,27 @@
+// #include <stdio.h>
+
+// int main() {
+//     char s[100];
+//     int i,length;
+//     printf("Enter a string: ");
+//     scanf("%s", s);
+    
+//     for(i = 0; s[i] != '\0'; i++);
+
+//     printf("The count of alphabet is %d: ",i);
+   
+
+//     return 0;
+// }
+
+
 #include <stdio.h>
 
 int main() {
     char s[100];
     int i,length;
     printf("Enter a string: ");
-    scanf("%s", s);
+    scanf("%[^\n]", s);
     
     for(i = 0; s[i] != '\0'; i++);
 
